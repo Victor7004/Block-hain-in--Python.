e69c42b25ca786c58b5ad9fbaab408e475e807a9
Victor7004 <viktoraleksejcuk@gmail.com>
2021-11-30 19:28:27 +0200

    Update def_sing_transaction.py

--- def_sing_transaction.py
+++ def_sing_transaction.py
@@ -29,3 +29,12 @@ def get_key(fp):
         key.append(rsa.PublicKey.load_pkcs1(file.read().encode(), 'PEM'))
     with open(prk, 'r') as file:
         key.append(rsa.PrivateKey.load_pkcs1(file.read().encode(), 'PEM'))
+
+        
+#Проверка подписи транзакции в python.
+
+def verify(transaction, signature):
+    key = transaction['user']['public_key']
+    print(str(transaction).encode())
+    print(signature)
+    return rsa.verify(message=str(transaction).encode(), signature=signature, pub_key=key)
